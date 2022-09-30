8631d9e980831b3d8fe2c2a8dc966d0c4b89eb0b
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-09-30 21:54:23 +0300

    debugging

--- app.py
+++ app.py
@@ -35,13 +35,7 @@ async def get_page_screenshot(request: Request):
                 """
             return Response(
                 status_code=200,
-                headers={"Content-Type": "text/html"},
-                content=data)
-            """
-            return Response(
-                status_code=200,
                 content=json.dumps({"status": "success", "cookies": data}))
-            """
         return Response(
             status_code=400,
             headers={"Content-Type": "application/json"},

cfe23ffbb051e295e32a180cabb347daca6cbfb5
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-10-02 17:21:53 +0300

    changed to firefox

--- arlupdater.py
+++ arlupdater.py
@@ -9,8 +9,7 @@ DEEZER_REDIRECT_URLS = ("https://www.deezer.com/en/", "https://www.deezer.com/en
 async def update_deezer_arl(login_mail, login_password):
     async with async_playwright() as playwright:
         try:
-            #? Chromium is used because for unknown reason, Firefox doesn't work
-            browser = await playwright.chromium.launch(headless=True)
+            browser = await playwright.firefox.launch(headless=True)
 
             page = await browser.new_page()
             await page.goto(DEEZER_LOGIN_URL)
